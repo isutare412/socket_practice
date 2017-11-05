7d1d5f7d53b95d848cb190dec8cda75daf877a8f
Redshore <isutare412@gmail.com>
2017-11-05 17:27:44 +0900

    gethostbyname, gethostbyaddr

---
+++ linux/16_gethostbyaddr.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+void handle_error(const char* msg);
+
+int main(int argc, const char* argv[])
+{
+    if (argc != 2) {
+        printf("Usage: %s <IP>\n", argv[0]);
+        exit(1);
+    }
+
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_addr.s_addr = inet_addr(argv[1]);
+
+    struct hostent* host;
+    host = gethostbyaddr((char*)&addr.sin_addr, 4, AF_INET);
+    if (!host) {
+        herror("gethostbyaddr() error");
+        exit(1);
+    }
+
+    printf("Official name: %s\n", host->h_name);
+    for (int i = 0; host->h_aliases[i]; ++i) {
+        printf("Aliases[%d]: %s\n", i + 1, host->h_aliases[i]);
+    }
+    printf("Address type: %s\n", (host->h_addrtype == AF_INET) ? "AF_INET" : "AF_INET6");
+    for (int i = 0; host->h_addr_list[i]; ++i) {
+        printf("IP addr[%d]: %s\n", i + 1, inet_ntoa(*(struct in_addr*)host->h_addr_list[i]));
+    }
+
+    return 0;
+}
+
+void handle_error(const char* msg)
+{
+    perror(msg);
+    exit(1);
+}
