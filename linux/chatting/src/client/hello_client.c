b5c6fc13dadd2dd5f3696eeccb47ecb31782743b
Redshore <isutare412@gmail.com>
2019-03-09 17:56:30 +0900

    added test cmake project

---
+++ linux/chatting/src/client/hello_client.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <common/test.h>
+
+int main()
+{
+    printf("I'm client\n");
+    test();
+
+    return 0;
+}
---
+++ linux/chatting/src/server/hello_server.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <common/test.h>
+
+int main()
+{
+    printf("I'm server\n");
+    test();
+
+    return 0;
+}
