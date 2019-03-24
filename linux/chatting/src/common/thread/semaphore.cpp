042cb97db0b306d1e117e22f6b67fe50073f65ff
Redshore <isutare412@gmail.com>
2019-03-24 15:09:58 +0900

    added semaphore

---
+++ linux/chatting/src/common/thread/semaphore.cpp
@@ -0,0 +1,47 @@
+#include "semaphore.hpp"
+
+namespace RS
+{
+
+Semaphore::Semaphore(
+    int64_t initial_count
+) noexcept
+    : m_count(initial_count)
+{
+}
+
+void
+Semaphore::notify() noexcept
+{
+    std::lock_guard<decltype(m_mutex)> lock(m_mutex);
+
+    ++m_count;
+    m_condition.notify_one();
+}
+
+void
+Semaphore::wait() noexcept
+{
+    std::unique_lock<decltype(m_mutex)> lock(m_mutex);
+
+    while (m_count <= 0)
+    {
+        m_condition.wait(lock);
+    }
+    --m_count;
+}
+
+bool
+Semaphore::try_wait() noexcept
+{
+    std::unique_lock<decltype(m_mutex)> lock(m_mutex);
+
+    if (m_count > 0)
+    {
+        --m_count;
+        return true;
+    }
+    return false;
+}
+
+} // RS
---
+++ linux/chatting/src/common/thread/semaphore.hpp
@@ -0,0 +1,35 @@
+#ifndef __SEMAPHORE_HPP__
+#define __SEMAPHORE_HPP__
+
+#include <mutex>
+#include <condition_variable>
+
+namespace RS
+{
+
+class Semaphore
+{
+public:
+    Semaphore(
+        int64_t initial_count
+    ) noexcept;
+
+public:
+    void
+    notify() noexcept;
+
+    void
+    wait() noexcept;
+
+    bool
+    try_wait() noexcept;
+
+private:
+    std::mutex m_mutex;
+    std::condition_variable m_condition;
+    int64_t m_count;
+};
+
+} // RS
+
+#endif // __SEMAPHORE_HPP__
